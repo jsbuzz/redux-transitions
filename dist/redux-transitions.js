69dd890315ae176532a3e57b9cf58e51744728bc
Matyas Buczko <whybuzz@gmail.com>
2020-11-01 01:31:14 +0000

    add dist to repo

---
+++ dist/redux-transitions.js
@@ -0,0 +1,145 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var react = require('react');
+var reactRedux = require('react-redux');
+
+const ACTION_LISTENERS = "_actionListeners";
+const thunkRX = /.*=>.*dispatch.*=>/i;
+
+const thunkKey = (thunkFn) => thunkFn.toString();
+
+const isThunk = (fn) => !!fn.toString().match(thunkRX);
+const asArray = (a) => (Array.isArray(a) ? a : [a]);
+const actionKey = (action) =>
+  typeof action === "function" ? thunkKey(action) : action.type;
+const actionTypeKey = (actionType) =>
+  typeof actionType === "function" ? thunkKey(actionType()) : actionType;
+
+// you can use this property in your actions to mark them
+// so the middleware will not propagate them to your reducers
+const STOP_PROPAGATION = "_stopPropagation";
+
+const createActionListener = () => {
+  const context = {};
+
+  return {
+    // redux middleware that will call the action listeners
+    actionListener: () => (next) => (action) => {
+      const key = actionKey(action);
+      const listeners = context.store[ACTION_LISTENERS][key];
+      if (listeners) {
+        listeners.forEach((listener) => listener(action));
+      }
+
+      if (action[STOP_PROPAGATION]) return;
+
+      return next(action);
+    },
+
+    // there is no other way to access the store on the listener level
+    setStore: (store) => {
+      context.store = store;
+    },
+  };
+};
+
+const processListeners = (listeners, actionCallback) => {
+  listeners.reduce((actions, current) => {
+    if (typeof current !== "function" || isThunk(current)) {
+      return [...actions, current];
+    }
+    actions.forEach((actionType) =>
+      actionCallback(actionTypeKey(actionType), current)
+    );
+    return [];
+  }, []);
+};
+
+// hook for setting listeners on actions
+function useActionListeners(...listeners) {
+  const store = reactRedux.useStore();
+  if (!store[ACTION_LISTENERS]) {
+    store[ACTION_LISTENERS] = {};
+  }
+  react.useEffect(() => {
+    processListeners(listeners, (event, callback) => {
+      const actionListeners = store[ACTION_LISTENERS][event];
+      if (actionListeners) {
+        actionListeners.push(callback);
+      } else {
+        store[ACTION_LISTENERS][event] = [callback];
+      }
+    });
+
+    return function cleanup() {
+      processListeners(listeners, (event, callback) => {
+        const actionListeners = store[ACTION_LISTENERS][event];
+        actionListeners.splice(actionListeners.indexOf(callback), 1);
+      });
+    };
+  }, []);
+}
+
+// hook for a standard fetch operation with pending and error states
+function usePendingState({
+  pending,
+  success,
+  failure,
+  failureHandler = (e) => e,
+}) {
+  const [state, setState] = react.useState({
+    pending: false,
+    error: null,
+  });
+
+  useActionListeners(
+    ...asArray(pending),
+    () =>
+      setState({
+        pending: true,
+        error: null,
+      }),
+
+    ...asArray(success),
+    () =>
+      setState({
+        pending: false,
+        error: null,
+      }),
+
+    ...asArray(failure),
+    (errorAction) =>
+      setState({
+        pending: false,
+        error: failureHandler(errorAction),
+      })
+  );
+
+  return [state.pending, state.error];
+}
+
+// hook for transitions
+const useTransitions = (transitionStates, transitionReducer) => {
+  const [state, setState] = react.useState({});
+
+  let listeners = [];
+  Object.keys(transitionStates).forEach((transition) => {
+    listeners = [
+      ...listeners,
+      ...asArray(transitionStates[transition]),
+      (action) => setState(transitionReducer(transition, action)),
+    ];
+  });
+
+  useActionListeners(...listeners);
+
+  return state;
+};
+
+exports.STOP_PROPAGATION = STOP_PROPAGATION;
+exports.createActionListener = createActionListener;
+exports.useActionListeners = useActionListeners;
+exports.usePendingState = usePendingState;
+exports.useTransitions = useTransitions;
